f2725214d697e888c3bba895279d55a6a9442b50
SeongChan Lee <foriequal@gmail.com>
2026-04-19 21:09:18 +0900

    Add meson build

---
+++ build-aux/cargo-build-artifact-dir-shim.py
@@ -0,0 +1,31 @@
+import dataclasses
+from typing import List
+import os
+import shutil
+import subprocess
+import sys
+
+@dataclasses.dataclass
+class Args:
+    cargo: List[str]
+    output: str
+    artifact_dir: str
+
+def parse_args(argv) -> Args:
+    cargo = []
+    it = iter(argv)
+    for arg in it:
+        if arg == "--output":
+            output = next(it)
+            continue
+        if arg == "--artifact-dir":
+            artifact_dir = next(it)
+            continue
+        cargo.append(arg)
+    return Args(cargo, output, artifact_dir)
+
+args = parse_args(sys.argv[1:])
+output_name = os.path.basename(args.output)
+
+subprocess.run(args.cargo, check=True)
+shutil.copy(args.output, os.path.join(args.artifact_dir, output_name))
