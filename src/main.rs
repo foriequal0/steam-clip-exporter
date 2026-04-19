825b4eab98f742c9f19772f105aa28491f19c09b
SeongChan Lee <foriequal@gmail.com>
2026-04-19 23:26:28 +0900

    Remove empty css

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use adw::Application;
 use adw::prelude::*;
-use gtk::gdk::Display;
+use gtk::gio;
 use gtk::glib;
-use gtk::{CssProvider, gio};
 
 pub mod clip_info_boxed;
 mod widgets;
@@ -16,20 +15,8 @@ fn main() -> glib::ExitCode {
         .expect("Failed to register resources");
 
     let app = Application::builder().application_id(APP_ID).build();
-    app.connect_startup(|_| load_css());
     app.connect_activate(build_ui);
     app.run()
-}
-
-fn load_css() {
-    let provider = CssProvider::new();
-    provider.load_from_resource("/io/github/foriequal0/steam-clip-exporter/style.css");
-
-    gtk::style_context_add_provider_for_display(
-        &Display::default().expect("Could not connect to a display."),
-        &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-    );
 }
 
 fn build_ui(app: &Application) {
