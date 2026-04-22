b91e5c7b4d481f714c664dbeecdc530de2f67265
SeongChan Lee <foriequal@gmail.com>
2026-04-22 17:09:39 +0900

    Fix windows icons

--- resources/convert-icons.sh
+++ resources/convert-icons.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 readonly SVG=io.github.foriequal0.SteamClipExporter.svg
 readonly OUTPUT="icons.ico"
-magick -background none "$SVG" -define icon:auto-resize=16,32,48,,128,256 -compress zip "$OUTPUT"
+magick -background none "$SVG" -define icon:auto-resize=16,32,48,64,128,256 -compress zip "$OUTPUT"
